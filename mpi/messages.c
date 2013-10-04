f16eaa5c8370a438b4bb241214c868125555c77b
bodin884 <bodin884@bcebbdc2-c0b6-9da2-c0bb-793a14a2c0d6>
2013-10-04 14:15:23 +0000

    initial import

---
+++ mpi/messages.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <mpi.h>
+
+#define  MASTER   0
+
+//return the wallclock time in seconds
+double wallclock(){
+  struct timeval tv;
+  double t;
+
+  gettimeofday(&tv, NULL);
+
+  t = (double)tv.tv_sec;
+  t += ((double)tv.tv_usec)/1000000.0;
+  return t;
+}
+
+int main(int argc, char *argv[]){
+  int   numtasks, taskid, len;
+  char hostname[MPI_MAX_PROCESSOR_NAME];
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+  MPI_Get_processor_name(hostname, &len);
+  printf ("Hello from task %d on %s!\n", taskid, hostname);
+  if (taskid == MASTER)
+    printf("MASTER: Number of MPI tasks is: %d\n",numtasks);
+  MPI_Finalize();
+  return 0;
+}

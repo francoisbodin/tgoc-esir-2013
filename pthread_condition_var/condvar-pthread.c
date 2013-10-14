8bb65a7c54f854a60fd7b024c13e32e13d4e7730
bodin884 <bodin884@bcebbdc2-c0b6-9da2-c0bb-793a14a2c0d6>
2013-10-14 15:27:46 +0000

    initial import

---
+++ pthread_condition_var/condvar-pthread.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int count = 0;
+
+int consume( void )
+{
+  int i = 10;
+  while( i-- ){
+      pthread_mutex_lock( &mutex );
+      //wait for a value to be produced
+      pthread_cond_wait( &cond, &mutex );
+      printf( "Consumed %d\n", count );
+      pthread_cond_signal( &cond );      
+      pthread_mutex_unlock( &mutex );
+    }
+  return( 0 );
+}
+
+void*  produce( void * arg )
+{
+  int i = 10;
+  while( i--){
+      pthread_mutex_lock( &mutex );
+      count++;
+      printf( "Produced %d\n", count);
+      // has produced a value (count++) now free the consumer
+      pthread_cond_signal( &cond );      
+      // wait for the consumer to be done
+      pthread_cond_wait( &cond, &mutex );
+      pthread_mutex_unlock( &mutex );
+    }
+  return( 0 );
+}
+
+int main( void )
+{
+  pthread_t mytask_thread;
+  pthread_create(&mytask_thread, NULL, &produce, NULL );
+  consume();
+  return 0;
+}
+
